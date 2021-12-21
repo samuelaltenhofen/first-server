4009ed53783164a69468e2d9753d641161ca63c6
samuelaltenhofen <94705394+samuelaltenhofen@users.noreply.github.com>
2021-12-21 10:59:11 -0300

    Add files via upload

---
+++ src/app/photos/photo/photo.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+
+})
+
+
+
+export class PhotoComponent {
+    @Input() description = '';
+    @Input() url = '';
+    
+
+}
